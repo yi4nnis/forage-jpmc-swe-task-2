cfb01b20137de201e3763d024d7b9c83963595bb
yi4nnis <yiannis.shamtani@gmail.com>
2024-10-05 01:28:00 +0100

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,8 +31,8 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {}
-    if (this.state.showGraph) {
-      return (<Graph data={this.state.data}/>)
+    if (this,state.showGraph)
+      return (<Graph data={this,state.data}/>)
     }
   }
 
